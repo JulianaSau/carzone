988ec00e1090978676b8f3b93cd117b6fb982c99
Juliana <julienyange@gmail.com>
2025-01-12 17:15:28 +0300

    create car and engine handlers

--- handler/car/car.go
+++ handler/car/car.go
@@ -1,1 +1,202 @@
 package car
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/JulianaSau/carzone/models"
+	"github.com/JulianaSau/carzone/service"
+	"github.com/gorilla/mux"
+)
+
+type CarHandler struct {
+	service service.CarServiceInterface
+}
+
+func NewCarHandler(service service.CarServiceInterface) *CarHandler {
+	return &CarHandler{
+		service: service,
+	}
+}
+
+func (h *CarHandler) GetCarById(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	// get the request params
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	// get the car by id from the car service
+	res, err := h.service.GetCarById(ctx, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error getting car by id: ", err)
+		return
+	}
+
+	// marshal the response
+	body, err := json.Marshal(res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling car by id response: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, err = w.Write(body)
+	if err != nil {
+		log.Println("Error writing response body: ", err)
+	}
+}
+
+func (h *CarHandler) GetCarByBrand(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	brand := r.URL.Query().Get("brand")
+	isEngine := r.URL.Query().Get("isEngine") == "true"
+
+	resp, err := h.service.GetCarByBrand(ctx, brand, isEngine)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error getting car by brand: ", err)
+		return
+	}
+
+	// marshal the response
+	body, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling car by brand response: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, err = w.Write(body)
+	if err != nil {
+		log.Println("Error writing response body: ", err)
+	}
+}
+
+func (h *CarHandler) CreateCar(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error reading request body: ", err)
+		return
+	}
+
+	var carReq models.CarRequest
+	err = json.Unmarshal(body, &carReq)
+	if err != nil {
+		log.Println("Error unmarshalling car request: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// create the car
+	createdCar, err := h.service.CreateCar(ctx, &carReq)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error creating car: ", err)
+		return
+	}
+
+	// marshal the response
+	responseBody, err := json.Marshal(createdCar)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling created car response: ", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	// write the response body
+	_, err = w.Write(responseBody)
+	if err != nil {
+		log.Println("Error writing response body: ", err)
+	}
+}
+
+func (h *CarHandler) UpdateCar(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error reading request body: ", err)
+		return
+	}
+
+	var carReq models.CarRequest
+	err = json.Unmarshal(body, &carReq)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error unmarshalling car request: ", err)
+		return
+	}
+
+	// update the car
+	updatedCar, err := h.service.UpdateCar(ctx, id, &carReq)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error updating car: ", err)
+		return
+	}
+
+	// marshal the response
+	responseBody, err := json.Marshal(updatedCar)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling updated car response body: ", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, err = w.Write(responseBody)
+}
+
+func (h *CarHandler) DeleteCar(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	// delete the car
+	deletedCar, err := h.service.DeleteCar(ctx, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error deleting car: ", err)
+		return
+	}
+
+	// marshal the response
+	responseBody, err := json.Marshal(deletedCar)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling deleted car response body: ", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, err = w.Write(responseBody)
+	if err != nil {
+		log.Println("Error writing response body: ", err)
+		return
+	}
+}
--- handler/engine/engine.go
+++ handler/engine/engine.go
@@ -1,1 +1,198 @@
 package engine
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/JulianaSau/carzone/models"
+	"github.com/JulianaSau/carzone/service"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type EngineHandler struct {
+	service service.EngineServiceInterface
+}
+
+func NewEngineHandler(service service.EngineServiceInterface) *EngineHandler {
+	return &EngineHandler{
+		service: service,
+	}
+}
+
+func (h *EngineHandler) GetEngineById(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	// get the request params
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	// get the engine by id from the engine service
+	res, err := h.service.GetEngineById(ctx, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error getting engine by id: ", err)
+		return
+	}
+
+	// marshal the response
+	body, err := json.Marshal(res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling engine response: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body'
+	_, err = w.Write(body)
+	if err != nil {
+		log.Println("Error writing response body: ", err)
+	}
+}
+
+func (h *EngineHandler) CreateEngine(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error reading request body: ", err)
+		return
+	}
+
+	var engineReq models.EngineRequest
+
+	// unmarshal the request body
+	err = json.Unmarshal(body, &engineReq)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("Error unmarshalling engine request: ", err)
+		return
+	}
+
+	// create the engine
+	createdEngine, err := h.service.CreateEngine(ctx, &engineReq)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error creating engine: ", err)
+		return
+	}
+
+	// marshal the response
+	responseBody, err := json.Marshal(createdEngine)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling engine response: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	// write the response body
+	_, err = w.Write(responseBody)
+	if err != nil {
+		log.Println("Error writing response body: ", err)
+	}
+}
+
+func (h *EngineHandler) UpdateEngine(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	// get the request params
+	params := mux.Vars(r)
+	id := params["id"]
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error reading request body: ", err)
+		return
+	}
+
+	var engineReq models.EngineRequest
+
+	// unmarshal the request body
+	err = json.Unmarshal(body, &engineReq)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("Error unmarshalling engine request: ", err)
+		return
+	}
+
+	// update the engine
+	updatedEngine, err := h.service.UpdateEngine(ctx, id, &engineReq)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error updating engine: ", err)
+		return
+	}
+
+	// marshal the response
+	responseBody, err := json.Marshal(updatedEngine)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling updated engine response: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	// write the response body
+	_, err = w.Write(responseBody)
+	if err != nil {
+		log.Println("Error writing response body: ", err)
+	}
+}
+
+func (h *EngineHandler) DeleteEngine(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	// get the request params
+	params := mux.Vars(r)
+	id := params["id"]
+
+	// delete the engine
+	deletedEngine, err := h.service.DeleteEngine(ctx, id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error deleting engine: ", err)
+		response := map[string]string{"error": "Invalid ID or engine not found"}
+		jsonResponse, _ := json.Marshal(response)
+		_, _ = w.Write(jsonResponse)
+		return
+	}
+
+	// check if engine was deleted properly
+	if deletedEngine.EngineID == uuid.Nil {
+		w.WriteHeader(http.StatusNotFound)
+		response := map[string]string{"error": "Engine not found"}
+		jsonResponse, _ := json.Marshal(response)
+		_, _ = w.Write(jsonResponse)
+		return
+	}
+
+	// marshal the response
+	responseBody, err := json.Marshal(deletedEngine)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marshalling deleted engine response: ", err)
+		response := map[string]string{"error": "Internal server error"}
+		jsonResponse, _ := json.Marshal(response)
+		_, _ = w.Write(jsonResponse)
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// write the response body
+	_, err = w.Write(responseBody)
+	if err != nil {
+		log.Println("Error writing response body: ", err)
+	}
+}
