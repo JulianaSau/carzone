756cc3c52dad9943992bb9c853d9acd21ebb9cc3
Juliana <julienyange@gmail.com>
2025-01-12 17:15:07 +0300

    fix car and engine models

--- models/car.go
+++ models/car.go
@@ -29,7 +29,7 @@ type CarRequest struct {
 	Price    float64 `json:"price"`
 }
 
-func validateRequest(carReq CarRequest) error {
+func ValidateRequest(carReq CarRequest) error {
 	if err := validateName(carReq.Name); err != nil {
 		return err
 	}
--- models/engine.go
+++ models/engine.go
@@ -19,34 +19,34 @@ type EngineRequest struct {
 	CarRange      int64 `json:"car_range"`
 }
 
-func validateEngineRequest(engineReq EngineRequest) error {
-	if err := validateDisplacement(engineReq.Displacement); err != nil {
+func ValidateEngineRequest(engineReq EngineRequest) error {
+	if err := ValidateDisplacement(engineReq.Displacement); err != nil {
 		return err
 	}
-	if err := validateNoOfCylinders(engineReq.NoOfCylinders); err != nil {
+	if err := ValidateNoOfCylinders(engineReq.NoOfCylinders); err != nil {
 		return err
 	}
-	if err := validateCarRanges(engineReq.CarRange); err != nil {
+	if err := ValidateCarRanges(engineReq.CarRange); err != nil {
 		return err
 	}
 	return nil
 }
 
-func validateDisplacement(displacement int64) error {
+func ValidateDisplacement(displacement int64) error {
 	if displacement <= 0 {
 		return errors.New("displacement must be greater than 0")
 	}
 	return nil
 }
 
-func validateNoOfCylinders(noOfCylinders int64) error {
+func ValidateNoOfCylinders(noOfCylinders int64) error {
 	if noOfCylinders <= 0 {
 		return errors.New("number of cylinders must be greater than 0")
 	}
 	return nil
 }
 
-func validateCarRanges(carRange int64) error {
+func ValidateCarRanges(carRange int64) error {
 	if carRange <= 0 {
 		return errors.New("car range must be greater than 0")
 	}
